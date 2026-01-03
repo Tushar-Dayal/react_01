6860013f66ef6f62dcd41d3a7a097ff551470833
Tushar Dayal <tushardayal.1711@gmail.com>
2026-01-03 17:23:45 +0530

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 id ='father'>child1</h1>
+      <h2 id ='grandfather'>child2</h2>
+    </div>
+    
+  )
+}
+
+export default App
+
+// rafce -> react arrow function export component
